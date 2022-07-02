6d165f1cee830a4839f6ed237805dbf6c8ececa3
redmaner <jakevdputten@protonmail.com>
2022-07-02 11:20:02 +0200

    list: Fix / add Find and HasMember + tests

--- list.go
+++ list.go
@@ -4,6 +4,7 @@ import "errors"
 
 var (
 	ErrIndexOutOfBounds = errors.New("index out of bounds")
+	ErrNotFound         = errors.New("not found")
 )
 
 type List[D comparable] struct {
@@ -16,7 +17,7 @@ func NewList[D comparable]() *List[D] {
 	return &List[D]{}
 }
 
-func (l *List[D]) ReadAtIndex(index int) (D, bool) {
+func (l *List[D]) ReadAtIndex(index int) (D, error) {
 	cursor := 0
 	currentNode := l.first
 
@@ -27,26 +28,34 @@ func (l *List[D]) ReadAtIndex(index int) (D, bool) {
 
 	if currentNode == nil {
 		var noData D
-		return noData, false
+		return noData, ErrIndexOutOfBounds
 	}
 
-	return currentNode.data, true
+	return currentNode.data, nil
 }
 
-func (l *List[D]) Find(data D) (index int, found bool) {
+func (l *List[D]) Find(data D) (int, error) {
 	cursor := 0
 	currentNode := l.first
 
 	for currentNode != nil {
 		if currentNode.data == data {
-			return cursor, true
+			return cursor, nil
 		}
+
+		cursor++
+		currentNode = currentNode.next
 	}
 
-	return 0, false
+	return 0, ErrNotFound
 }
 
-func (l *List[D]) Prepend(data D) {
+func (l *List[D]) HasMember(data D) bool {
+	_, err := l.Find(data)
+	return err == nil
+}
+
+func (l *List[D]) Prepend(data D) *List[D] {
 	newNode := NewNode[D]().setData(data).setNext(l.first)
 
 	if l.last == nil {
@@ -55,20 +64,24 @@ func (l *List[D]) Prepend(data D) {
 
 	l.length++
 	l.first = newNode
+
+	return l
 }
 
-func (l *List[D]) Append(data D) {
+func (l *List[D]) Append(data D) *List[D] {
 	newNode := NewNode[D]().setData(data)
 
 	if l.first == nil && l.last == nil {
 		l.first = newNode
 		l.last = newNode
-		return
+		return l
 	}
 
 	l.length++
 	l.last.next = newNode
 	l.last = newNode
+
+	return l
 }
 
 func (l *List[D]) InsertAtIndex(index int, data D) error {
--- list_test.go
+++ list_test.go
@@ -9,28 +9,28 @@ import (
 func TestListReadAndInsertion(t *testing.T) {
 
 	// Basic creation and insertion at tail
-	list := New[string]()
+	list := NewList[string]()
 	list.Append("1") // index 0
 	list.Append("2") // index 1
 	list.Append("4") // index 2
 
 	var value interface{}
-	var ok bool
+	var err error
 
 	assert := assert.New(t)
 
 	// Basic reading at indexes
-	if value, ok = list.ReadAtIndex(0); !ok {
+	if value, err = list.ReadAtIndex(0); err != nil {
 		t.Fatal("Data not found")
 	}
 	assert.Equal(value, "1", "Expected 1")
 
-	if value, ok = list.ReadAtIndex(1); !ok {
+	if value, err = list.ReadAtIndex(1); err != nil {
 		t.Fatal("Data not found")
 	}
 	assert.Equal(value, "2", "Expected 2")
 
-	if value, ok = list.ReadAtIndex(2); !ok {
+	if value, err = list.ReadAtIndex(2); err != nil {
 		t.Fatal("Data not found")
 	}
 	assert.Equal(value, "4", "Expected 4")
@@ -40,12 +40,12 @@ func TestListReadAndInsertion(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if value, ok = list.ReadAtIndex(2); !ok {
+	if value, err = list.ReadAtIndex(2); err != nil {
 		t.Fatal("Data not found")
 	}
 	assert.Equal(value, "3", "Expected 3")
 
-	if value, ok = list.ReadAtIndex(3); !ok {
+	if value, err = list.ReadAtIndex(3); err != nil {
 		t.Fatal("Data not found")
 	}
 	assert.Equal(value, "4", "Expected 4")
@@ -54,4 +54,30 @@ func TestListReadAndInsertion(t *testing.T) {
 	if err := list.InsertAtIndex(10, "nope"); err != ErrIndexOutOfBounds {
 		t.Fatal("Insertion at index out of bounds should not work")
 	}
+}
+
+func TestFindAndHasMember(t *testing.T) {
+	assert := assert.New(t)
+
+	// Basic creation and insertion at tail
+	list := NewList[string]().Append("1").Append("2").Append("3").Append("4").Append("5")
+
+	// Test has member
+	assert.Equal(list.HasMember("5"), true, "5 should be present in the list")
+	assert.Equal(list.HasMember("10"), false, "10 should not be present in the list")
+
+	// Test find
+	index, err := list.Find("5")
+	if err == ErrNotFound {
+		t.Fatal("5 should be present in the list")
+	}
+
+	assert.Equal(index, 4, "Index of 5 should be 4")
+
+	// Test read at index
+	val, err := list.ReadAtIndex(4)
+	if err == ErrIndexOutOfBounds {
+		t.Fatal("Index 4 should exist")
+	}
+	assert.Equal(val, "5", "Index 4 should have the value 5")
 }
